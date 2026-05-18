c645171547793cb905ed4d1497a12c0d6d12fd0d
erichiroshi <erichiroshi@hotmail.com>
2026-05-18 13:39:46 -0400

    Projeto criado

---
+++ src/main/java/com/erichiroshi/speechaihexagonal/SpeechAiHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.erichiroshi.speechaihexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechAiHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeechAiHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erichiroshi/speechaihexagonal/SpeechAiHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erichiroshi.speechaihexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechAiHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
